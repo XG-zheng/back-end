f24d0aee79480c19fc7b5e6f287eff868e9e1bb4
Q1176011838 <34544283+Q1176011838@users.noreply.github.com>
2019-11-14 14:33:43 +0800

    Update r_message.php
    未读消息发送后即删除
    需要前端传json格式的userid过来

--- messgae/r_message.php
+++ messgae/r_message.php
@@ -1,15 +1,31 @@
 <?php
+header('Content-Type:text/html;Charset=utf-8');
+header("Access-Control-Allow-Origin: *");
 
-	function  conn(){
-	$dns = "mysql:host=47.95.255.31;dbname=pethome";
-	return new PDO($dns, "root","123456");
-	}
+
+$data = file_get_contents('php://input');
+$message = json_decode($data,true);
 	
+//$message['userid'] = 2;
+
+	
+	
+	//获得消息
 	$db = conn();
-	//$sql = 'insert into unsend_affairs(type,send_id,receive_id,message) values(0,1,4,"can you hear me?")';
-	//$sql = 'insert into unsend_affairs(type,send_id,receive_id,title) values(1,3,2,"我这条狗真好看！")';
-	$sql = 'select * from unsend_affairs';
+	$sql = 'select * from unsend_affairs where receive_id = :userid';
 	$stmt = $db->prepare($sql);
-	$stmt->execute();
+	$stmt->execute([':userid'=>$message['userid']]);
 	$res = $stmt->fetchAll();
 	echo json_encode($res); 
+	//删除消息
+	$sql = 'delete from unsend_affairs where receive_id = :userid';
+	$stmt = $db->prepare($sql);
+	$stmt->execute([':userid'=>$message['userid']]);
+
+function  conn(){
+	$dns = "mysql:host=47.95.255.31;dbname=pethome";
+	return new PDO($dns, "root","123456");
+}
+
+
+
